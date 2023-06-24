1ab05de02d2b7048444245294baebcc4e2ef5804
I Shan Liou <ishanliou@13s-MacBook-Pro.local>
2023-06-23 18:02:01 -0700

    portfolio init

---
+++ components/Main.jsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+// import { AiOutlineMail } from "react-icons/ai";
+// import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaLinkedinIn } from "react-icons/fa";
+import { gsap } from "gsap";
+
+const Main = () => {
+  let tl = gsap.timeline();
+
+  useEffect(() => {
+    tl.to(".animation", {
+      duration: 1,
+      autoAlpha: 1,
+      opacity: 1,
+      y: -60,
+      ease: "power1.inOut",
+      stagger: {
+        each: 0.1,
+        ease: "sine.inOut",
+      },
+    });
+  }, [tl]);
+  return (
+    <div id="main" className="w-full h-screen text-center ">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center ">
+        <div>
+          <p className="uppercase text-sm md:text-l tracking-widest text-gray-400 animation">
+            Let's build something together
+          </p>
+
+          <h1 className="py-4 text-gray-700 tracking-widest animation ">
+            this is <span className="text-[#5651e5] animate-text">ishan</span>
+          </h1>
+
+          <p className="uppercase text-md md:text-xl tracking-widest text-gray-600 animation">
+            A Front-End Developer
+          </p>
+          <div className="flex items-center justify-center max-w-[330px] m-auto py-4 animation">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <a
+                href="https://www.linkedin.com/in/ishanliou/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
